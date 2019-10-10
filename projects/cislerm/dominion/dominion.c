7e5a3028c1e50c8a0977dc9b4e791d91e1545b19
Melanie Cisler <cislerm@oregonstate.edu>
2019-10-10 13:46:41 -0500

    added in mineEffect()
    used case mine: to fill code

--- projects/cislerm/dominion/dominion.c
+++ projects/cislerm/dominion/dominion.c
@@ -910,7 +910,45 @@ int tributeEffect(struct gameState *state, int choice1, int choice2, int handPos
         }
 
         return 0;
-	
+
+//adding in int mineEffect function
+int mineEffect(struct gameState *state, int choice1, int choice2, int handPos, int currentPlayer){
+//copy mine info from case statement
+        int j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+        if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+        {
+            return -1;
+        }
+
+        if (choice2 > treasure_map || choice2 < curse)
+        {
+            return -1;
+        }
+
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+        {
+            return -1;
+        }
+
+        gainCard(choice2, state, 2, currentPlayer);
+
+        //discard card from hand
+        discardCard(handPos, currentPlayer, state, 0);
+
+        //discard trashed card
+        for (i = 0; i < state->handCount[currentPlayer]; i++)
+        {
+            if (state->hand[currentPlayer][i] == j)
+            {
+                discardCard(i, currentPlayer, state, 0);
+                break;
+            }
+        }
+
+        return 0;
+
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
     int i;
