fea6a92f05c464350e6cc6a1e16ee098d40a238a
Melanie Cisler <cislerm@oregonstate.edu>
2019-10-10 13:23:48 -0500

    added minionEffect() function
    copied case minion: information and added it to minionEffect function

--- projects/cislerm/dominion/dominion.c
+++ projects/cislerm/dominion/dominion.c
@@ -737,6 +737,58 @@ int baronEffect(struct gameState *state, int choice1, int choice2, int handPos, 
 
         return 0;
 
+//added in minionEffect function
+int minionEffect(struct gameState *state, int choice1, int choice2, int handPos, int currentPlayer){
+	//copy minion info from case statement 
+	//+1 action
+        state->numActions++;
+
+        //discard card from hand
+        discardCard(handPos, currentPlayer, state, 0);
+
+		if (choice1)
+        {
+            state->coins = state->coins + 2;
+        }
+        else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+        {
+            //discard hand
+            while(numHandCards(state) > 0)
+            {
+                discardCard(handPos, currentPlayer, state, 0);
+            }
+
+            //draw 4
+            for (i = 0; i < 4; i++)
+            {
+                drawCard(currentPlayer, state);
+            }
+
+            //other players discard hand and redraw if hand size > 4
+            for (i = 0; i < state->numPlayers; i++)
+            {
+                if (i != currentPlayer)
+                {
+                    if ( state->handCount[i] > 4 )
+                    {
+                        //discard hand
+                        while( state->handCount[i] > 0 )
+                        {
+                            discardCard(handPos, i, state, 0);
+                        }
+
+                        //draw 4
+                        for (j = 0; j < 4; j++)
+                        {
+                            drawCard(i, state);
+                        }
+                    }
+                }
+            }
+
+        }
+        return 0;
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
     int i;
